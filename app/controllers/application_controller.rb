13630ca62f8e7ff440ffd08f59d3bc4d0206ee14
Sarat Kumar Kalagara <sarat@Serivces-MacBook-Pro.local>
2026-01-20 16:31:25 +0530

    protected APIs and handleauthorization errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
     before_action :authenticate_user!
     include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError do
+    render json: { error: "Not authorized" }, status: :forbidden
 end
