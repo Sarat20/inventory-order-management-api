28fed9f7b939275bc87bb7025d5feeae5716af29
Sarat Kumar Kalagara <sarat@Serivces-MacBook-Pro.local>
2026-01-21 01:27:06 +0530

    secured suppliers with policy authorization

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -1,28 +1,39 @@
 module Api
   module V1
     class SuppliersController < BaseController
+      before_action :set_supplier, only: %i[update destroy]
 
       def index
+        authorize Supplier
         render json: { success: true, data: Supplier.all }
       end
 
       def create
-        supplier = Supplier.create!(supplier_params)
+        supplier = Supplier.new(supplier_params)
+        authorize supplier
+        supplier.save!
+
         render json: { success: true, data: supplier }, status: :created
       end
 
       def update
-        supplier = Supplier.find(params[:id])
-        supplier.update!(supplier_params)
-        render json: { success: true, data: supplier }
+        authorize @supplier
+        @supplier.update!(supplier_params)
+
+        render json: { success: true, data: @supplier }
       end
 
       def destroy
-        Supplier.find(params[:id]).destroy
+        authorize @supplier
+        @supplier.destroy
         render json: { success: true }
       end
 
       private
+
+      def set_supplier
+        @supplier = Supplier.find(params[:id])
+      end
 
       def supplier_params
         params.require(:supplier).permit(:name, :email)
---
+++ app/policies/supplier_policy.rb
@@ -0,0 +1,17 @@
+class SupplierPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.staff?
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+end
