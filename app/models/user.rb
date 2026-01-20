e7e08b19c5b1e2f192f9d4d35d17b8eb3f4a5bdf
Sarat Kumar Kalagara <sarat@Serivces-MacBook-Pro.local>
2026-01-20 16:25:07 +0530

    enabled JWT revocation throug JTI strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true
+
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   enum role: {
     admin: 0,
     staff: 1
